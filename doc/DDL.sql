46b24d40f3ac35e8273bba7c1dfbea58b11fdc74
MaxQiu <yueyiqiu178@gmail.com>
2020-01-31 17:50:49 +0800

    commit20200131

---
+++ doc/DDL.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `transaction` (
+  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock_id` varchar(45) DEFAULT NULL,
+  `transaction_type` varchar(1) DEFAULT NULL,
+  `transaction_price` decimal(10,3) DEFAULT NULL,
+  `transaction_amuont` int(11) DEFAULT NULL,
+  `transaction_date` date DEFAULT NULL,
+  `is_ settled` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `config` (
+  `config_key` varchar(45) NOT NULL,
+  `config_value` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`config_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
